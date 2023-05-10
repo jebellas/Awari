5334e1a2f7f928ffd1efed4a3c14c8708267339e
jessicaabbellas <jessica.bellas@fadami.com.br>
2023-05-09 21:08:42 -0300

    add paginas aula3 parte 2

--- 2.FORM-AWARI/scripts.js
+++ 2.FORM-AWARI/scripts.js
@@ -1,3 +1,21 @@
-console.log("teste");
+var form = document.getElementById("contact-form");
 
-alert("Hello");
+// criacao de evento para evitar reload da pagina - evitando perda de dados
+function handleSubmit(event) {
+    event.preventDefault();
+
+    var nome = form.elements["nome"].value;
+    var email = form.elements["email"].value;
+    var mensagem = form.elements["mensagem"].value;
+
+    //var resultado = "Nome: " + nome + "\n E-mail: " + email + "\n Mensagem: " + mensagem;
+    var resultado = `Nome: ${nome} \n E-mail: ${email} \n Mensagem: ${mensagem}`;
+
+    //console.log(nome, email, mensagem);
+    console.log(resultado);
+
+    alert(resultado);
+
+}
+
+form.addEventListener("submit", handleSubmit);
